e7a4c160797859f76df1c0d5360cc909f158a9c4
allelementary <allelementaryfor@gmail.com>
2023-01-14 18:44:23 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pybitcointools23',
-      version='0.1.0',
+      version='0.1.1',
       description='Python Crypto Coin Tools',
       long_description=open('README.md').read(),
       long_description_content_type="text/markdown",
